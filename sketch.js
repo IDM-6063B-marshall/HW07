e99bb76992ab078233dd3468a9438a415139c274
Jubilee Marshall <jam1703@nyu.edu>
2024-10-24 19:36:41 -0400

    finished text effects, readme

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-// let mString = "";
-
-// function preload() {
-//   mString = loadStrings("./Tove.txt");
+//let mString = "";
+//function preload() {
+//mString = loadStrings("./Tove.txt");
 // }
+//with a relatively short text I found it easier to load the strings directly in the sketch window
 
 // typewriter effect for mString0; bunched up text for mString1; mString2 linked to mouse
 
@@ -14,50 +14,47 @@ let mString2 = "I love you!";
 let charPos0 = 0;
 let charPos1 = 0;
 let charPos2 = 0;
+let targetWord = "eat";
+let velocity = 0.2;
+let acceleration = 0.005;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   //getting the length of the first two strings to set time delay
   print(mString0.length);
   print(mString1.length);
-  print(windowWidth)
 }
 
 function draw() {
-  //referred to https://editor.p5js.org/pippinbarr/sketches/bjxEfpiwS for some of the typewriter code
-
-  background(255, 255, 255); //bg here will prevent snail trail for type2 if desired
+  //I referred to https://editor.p5js.org/pippinbarr/sketches/bjxEfpiwS for scaffolding for some of the typewriter code
+  //background(255, 255, 255); //bg here will prevent snail trail for type2 if desired
   let type0 = mString0.substring(0, charPos0);
   textWrap(CHAR);
   textSize(24);
   textFont(`Courier`);
-  text(type0, 100, 100);
-  charPos0 += 0.5;
+  text(type0, 100, 100, windowWidth - windowWidth / 3);
+  charPos0 += 0.2;
 
-  // need to delay start and wrap text for mString1 and mString 2
-
-  // I want the list of things in mString1 to bunch up/collect at the right of the window
+  //delay start and wrap text for mString1 and mString 2
 
   let type1 = mString1.substring(0, charPos1);
   if (charPos0 >= 80) {
     textSize(24);
     textFont(`Courier`);
-    let lines1 = mString1.split("")
-    text(type1, 200, 200, windowWidth - windowWidth / 4);
-    charPos1 += 0.5;
-    
-    // need to find a way to express if the first line has hit the right edge of the text box
-    // if (charPos1 >= )
-    
+    text(type1, 200, 200, windowWidth - windowWidth / 3);
+    velocity += acceleration;
+    charPos1 += velocity;
 
-    //link type2 to mouse movement
+    //want mString1 to speed up to convey excitement; explored some code for velocity here: https://editor.p5js.org/Alizarin/sketches/1Gfsnas9A
+
+    //link type2 to mouse movement and crowd the screen w/ love
 
     let type2 = mString2.substring(0, charPos2);
-    if (charPos1 >= 285) {
-      textSize(24);
+    if (charPos1 >= 350) {
+      textSize(42);
       textFont(`Courier`);
       text(type2, mouseX, mouseY);
-      charPos2 += 0.5;
+      charPos2 += 1;
     }
   }
 }
